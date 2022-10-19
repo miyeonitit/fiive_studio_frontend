c76ac9599196687b5aa7a84722e3d7cbd65ef38a
miyeonitit <mypark@pureblack.co.kr>
2022-10-19 11:12:42 +0900

    👽️ 형식: API 요청 url 중 demo 제거

--- components/AnnouncementModal.tsx
+++ components/AnnouncementModal.tsx
@@ -1,65 +1,65 @@
-import { useState } from "react";
-import useStore from "../store/video";
-import { v4 as uuidv4 } from "uuid";
+import { useState } from 'react'
+import useStore from '../store/video'
+import { v4 as uuidv4 } from 'uuid'
 
 interface props {
-  toggle: () => void;
+  toggle: () => void
 }
 
 const AnnouncementModal = (props: props) => {
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-  const channel = useStore((state) => state.channel);
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+  const channel = useStore((state) => state.channel)
 
-  const [metadata, setMetadata] = useState("");
+  const [metadata, setMetadata] = useState('')
 
   const embedMetadata = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    if (channel === null) return;
+    if (channel === null) return
 
     if (metadata.length) {
       const form = new URLSearchParams({
         arn: channel.arn,
         data: JSON.stringify({
-          type: "ANNOUNCEMENT",
+          type: 'ANNOUNCEMENT',
           message: {
             id: uuidv4(),
             content: metadata,
           },
         }),
-      });
+      })
 
-      const resp = await fetch(`${baseUrl}/demo/channel/metadata`, {
-        method: "POST",
+      const resp = await fetch(`${baseUrl}/channel/metadata`, {
+        method: 'POST',
         body: form,
-      });
+      })
 
-      setMetadata("");
+      setMetadata('')
     }
-    props.toggle();
-  };
+    props.toggle()
+  }
 
   return (
-    <div className="announcement-modal">
-      <div className="wrapper">
-        <form onSubmit={embedMetadata} action="">
-          <div className="form-group">
+    <div className='announcement-modal'>
+      <div className='wrapper'>
+        <form onSubmit={embedMetadata} action=''>
+          <div className='form-group'>
             <input
               onInput={(e) => {
-                const target = e.target as HTMLInputElement;
-                setMetadata(target.value);
+                const target = e.target as HTMLInputElement
+                setMetadata(target.value)
               }}
               value={metadata}
-              type="text"
-              className="form-control mb-2"
+              type='text'
+              className='form-control mb-2'
             />
 
-            <button type="sumbit">공지 등록</button>
+            <button type='sumbit'>공지 등록</button>
           </div>
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AnnouncementModal;
+export default AnnouncementModal
--- components/Video.tsx
+++ components/Video.tsx
@@ -1,96 +1,96 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from 'react'
 // import Messages from "../components/Messages";
-import useStore from "../store/video";
+import useStore from '../store/video'
 
 const Video = () => {
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-  const ivsPlayer = useRef<HTMLVideoElement>(null);
-  const [init, setInit] = useState(false);
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+  const ivsPlayer = useRef<HTMLVideoElement>(null)
+  const [init, setInit] = useState(false)
   // const [messages, setMessages] = useState<any[]>([]);
 
-  const setChannel = useStore((state: any) => state.setChannel);
+  const setChannel = useStore((state: any) => state.setChannel)
 
-  const addAnnouncement = useStore((state: any) => state.addAnnouncement);
+  const addAnnouncement = useStore((state: any) => state.addAnnouncement)
 
-  const addQuestion = useStore((state: any) => state.addQuestion);
-  const resolveQuestion = useStore((state: any) => state.resolveQuestion);
+  const addQuestion = useStore((state: any) => state.addQuestion)
+  const resolveQuestion = useStore((state: any) => state.resolveQuestion)
 
-  const addReaction = useStore((state: any) => state.addReaction);
+  const addReaction = useStore((state: any) => state.addReaction)
 
-  const setTimer = useStore((state: any) => state.setTimer);
+  const setTimer = useStore((state: any) => state.setTimer)
 
   useEffect(() => {
-    initVideo();
-  });
+    initVideo()
+  })
 
   const fetchChannels = async () => {
-    const resp = await fetch(`${baseUrl}/demo/channel`, {
-      method: "GET",
-    });
+    const resp = await fetch(`${baseUrl}/channel`, {
+      method: 'GET',
+    })
 
-    const { channels = [] } = await resp.json();
+    const { channels = [] } = await resp.json()
 
     const channel = channels.find((item: any) => {
-      return !item.authrozied;
-    });
+      return !item.authrozied
+    })
 
-    return channel;
-  };
+    return channel
+  }
 
   const fetchChannel = async (arn: string) => {
     const params = new URLSearchParams({
       arn,
-    });
+    })
 
-    const resp = await fetch(`${baseUrl}/demo/channel?${params}`, {
-      method: "GET",
-    });
+    const resp = await fetch(`${baseUrl}/channel?arn=${params}`, {
+      method: 'GET',
+    })
 
-    const { channel = null } = await resp.json();
+    const { channel = null } = await resp.json()
 
-    setChannel(channel);
-    return channel;
-  };
+    setChannel(channel)
+    return channel
+  }
 
   const initVideo = async () => {
     // Bail if already initialized
-    if (init) return;
+    if (init) return
 
     // Bail if IVS sdk is not loaded
-    const { IVSPlayer } = window;
-    if (typeof IVSPlayer === "undefined") return;
+    const { IVSPlayer } = window
+    if (typeof IVSPlayer === 'undefined') return
 
     // Bail if player is not supported
-    if (!IVSPlayer.isPlayerSupported) return;
+    if (!IVSPlayer.isPlayerSupported) return
 
-    const { arn } = await fetchChannels();
-    const { playbackUrl } = await fetchChannel(arn);
+    const { arn } = await fetchChannels()
+    const { playbackUrl } = await fetchChannel(arn)
 
-    const player = IVSPlayer.create();
+    const player = IVSPlayer.create()
 
     // Handle embedded metadata events
     player.addEventListener(
       IVSPlayer.PlayerEventType.TEXT_METADATA_CUE,
       (cue: any) => {
-        const { text: json, startTime } = cue;
-        const { type, message } = JSON.parse(json);
+        const { text: json, startTime } = cue
+        const { type, message } = JSON.parse(json)
 
         switch (type) {
-          case "ANNOUNCEMENT":
-            addAnnouncement(message);
-            break;
-          case "QUESTION":
-            addQuestion(message);
-            break;
-          case "RESOLVE_QUESTION":
-            resolveQuestion(message);
-            break;
-          case "REACTION":
-            addReaction(message);
-            break;
-          case "TIMER":
-            setTimer(message);
-            break;
+          case 'ANNOUNCEMENT':
+            addAnnouncement(message)
+            break
+          case 'QUESTION':
+            addQuestion(message)
+            break
+          case 'RESOLVE_QUESTION':
+            resolveQuestion(message)
+            break
+          case 'REACTION':
+            addReaction(message)
+            break
+          case 'TIMER':
+            setTimer(message)
+            break
         }
 
         /*
@@ -103,35 +103,35 @@ const Video = () => {
         ]);
         */
       }
-    );
+    )
 
     player.addEventListener(IVSPlayer.PlayerEventType.ERROR, (error: any) => {
-      const { type = null } = error;
+      const { type = null } = error
 
       switch (type) {
-        case "ErrorNoSource":
-        case "ErrorNotAvailable":
+        case 'ErrorNoSource':
+        case 'ErrorNotAvailable':
           window.setTimeout(() => {
-            player.load(playbackUrl);
-            player.play();
-          }, 5000);
-          break;
+            player.load(playbackUrl)
+            player.play()
+          }, 5000)
+          break
       }
-    });
+    })
 
-    player.attachHTMLVideoElement(ivsPlayer.current);
-    player.load(playbackUrl);
-    player.play();
+    player.attachHTMLVideoElement(ivsPlayer.current)
+    player.load(playbackUrl)
+    player.play()
 
-    setInit(true);
-  };
+    setInit(true)
+  }
 
   return (
     <>
-      <video src="" ref={ivsPlayer} playsInline controls={true}></video>
+      <video src='' ref={ivsPlayer} playsInline controls={true}></video>
       {/* <Messages messages={messages} /> */}
     </>
-  );
-};
+  )
+}
 
-export default Video;
+export default Video
