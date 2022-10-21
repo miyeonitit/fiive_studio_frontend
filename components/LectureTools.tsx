b660a0858269ac429319da54f242bcd3a77df725
miyeonitit <mypark@pureblack.co.kr>
2022-10-21 18:43:27 +0900

    ✅ 형식: git actions yml 파일 추가 및 컴포넌트 내 경로 변경

--- components/LectureTools.tsx
+++ components/LectureTools.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import { useState } from "react";
-import useStore from "../store/video";
+import Image from 'next/image'
+import { useState } from 'react'
+import useStore from '../store/video'
 
 interface props {
-  openAnnouncementModal: () => void;
-  openTimerModal: () => void;
+  openAnnouncementModal: () => void
+  openTimerModal: () => void
 }
 
 const LectureTools = (props: props) => {
   const [questionMonitorWindow, setQuestionMonitorWindow] =
-    useState<Window | null>(null);
+    useState<Window | null>(null)
 
   // Subscribe to store
   useStore.subscribe(
@@ -17,58 +17,54 @@ const LectureTools = (props: props) => {
     (questions) => {
       questionMonitorWindow?.postMessage(
         questions.filter((item) => item.resolved === false)
-      );
+      )
     }
-  );
+  )
 
   const openChatMonitor = () => {
-    window.open(
-      "/fiive/chat-monitor",
-      "chat-monitor",
-      "width=340,height=500px"
-    );
-  };
+    window.open('/chat-monitor', 'chat-monitor', 'width=340,height=500px')
+  }
 
   const openQuestionMonitor = () => {
     const ref = window.open(
-      "/fiive/question-monitor",
-      "question-monitor",
-      "width=340,height=400px"
-    );
+      '/question-monitor',
+      'question-monitor',
+      'width=340,height=400px'
+    )
 
-    setQuestionMonitorWindow(ref);
-  };
+    setQuestionMonitorWindow(ref)
+  }
 
   return (
-    <div className="lecture-tools">
+    <div className='lecture-tools'>
       <h3>강의 도구</h3>
 
-      <ul className="buttons">
+      <ul className='buttons'>
         <li>
           <button
             onClick={() => {
-              props.openAnnouncementModal();
+              props.openAnnouncementModal()
             }}
-            type="button"
-            className="announcement"
+            type='button'
+            className='announcement'
           >
             <Image
-              src="/components/lecture-tools/announce.svg"
-              alt="Announcement"
-              width="24"
-              height="24"
+              src='/components/lecture-tools/announce.svg'
+              alt='Announcement'
+              width='24'
+              height='24'
             ></Image>
             <span>공지사항</span>
           </button>
         </li>
 
         <li>
-          <button onClick={openQuestionMonitor} type="button" className="qna">
+          <button onClick={openQuestionMonitor} type='button' className='qna'>
             <Image
-              src="/components/lecture-tools/qna_outlined.svg"
-              alt="Q&amp;A"
-              width="24"
-              height="24"
+              src='/components/lecture-tools/qna_outlined.svg'
+              alt='Q&amp;A'
+              width='24'
+              height='24'
             ></Image>
             <span>질의응답</span>
           </button>
@@ -77,42 +73,42 @@ const LectureTools = (props: props) => {
         <li>
           <button
             onClick={() => {
-              props.openTimerModal();
+              props.openTimerModal()
             }}
-            type="button"
-            className="timer"
+            type='button'
+            className='timer'
           >
             <Image
-              src="/components/lecture-tools/timer.svg"
-              alt="Timer"
-              width="24"
-              height="24"
+              src='/components/lecture-tools/timer.svg'
+              alt='Timer'
+              width='24'
+              height='24'
             ></Image>
             <span>타이머</span>
           </button>
         </li>
 
         <li>
-          <button onClick={openChatMonitor} type="button" className="chat">
+          <button onClick={openChatMonitor} type='button' className='chat'>
             <Image
-              src="/components/lecture-tools/comment_outlined.svg"
-              alt="Chat"
-              width="24"
-              height="24"
+              src='/components/lecture-tools/comment_outlined.svg'
+              alt='Chat'
+              width='24'
+              height='24'
             ></Image>
             <span>채팅</span>
           </button>
         </li>
       </ul>
 
-      <div className="add-tool-button">
-        <button type="button">
-          <img src="/components/lecture-tools/add.svg" alt="Add" />
+      <div className='add-tool-button'>
+        <button type='button'>
+          <img src='/components/lecture-tools/add.svg' alt='Add' />
           <span>강의 도구 추가</span>
         </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LectureTools;
+export default LectureTools
---
+++ pages/question-monitor.tsx
@@ -0,0 +1,17 @@
+// import Layout from "../../components/FiiveLearnerLayout";
+import { NextPageWithLayout } from '../types/NextPageWithLayout'
+import QuestionMonitor from '../components/QuestionMonitor'
+
+const QuestionMonitorPage: NextPageWithLayout = () => {
+  return (
+    <div className='fiive question-monitor page'>
+      <QuestionMonitor></QuestionMonitor>
+    </div>
+  )
+}
+
+// QuestionMonitorPage.getLayout = (page: ReactElement) => {
+//   return <Layout>{page}</Layout>;
+// };
+
+export default QuestionMonitorPage
