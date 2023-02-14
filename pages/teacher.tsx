f57bad52c631b8bef0b36581cf45a216ff568006
miyeonitit <mypark@pureblack.co.kr>
2023-02-14 17:55:58 +0900

    🐛 FIX: resolve confilct

--- pages/teacher.tsx
+++ pages/teacher.tsx
@@ -228,8 +228,6 @@ const TeacherPage: NextPageWithLayout = (props: props) => {
   }, [props.auth_token])
 
   // 최상단 Nav의 live 상태 표현을 위한, live 상태인지 아닌지 계속 판단해주는 로직
-  // setInterval은 state 갱신이 되지 않아, 페이지 렌더 전 초기의 빈 state만을 기억하여 콜백함수를 호출한다는 단점이 존재함
-  // 따라서 react custom hooks인 useInterval 사용
   useInterval(() => {
     getLiveStreamInfomation(props?.class_id)
   }, 5000)
